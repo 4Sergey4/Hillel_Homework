bae2b9bdf17559e52d747610d444feaa6b1b142e
4Sergey4 <98.sergey.bogdan@gmail.com>
2025-01-30 22:53:23 +0200

    Add Homework5

---
+++ HillelLesson5Homework5DivisionByZero/HillelLesson5Homework5DivisionByZero/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int firstNumber;
+int secondNumber;
+int result;
+while(true)
+{
+    Console.Write("Введіть перше число:");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введіть друге число:");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    try
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"{result}\n");
+    }
+    catch
+    {
+        Console.WriteLine("Помилка, на 0 ділити не можна\n");
+    }
+}

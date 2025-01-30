03c8f2d3ad94997d6f85be4c4466ffa0d319c374
4Sergey4 <98.sergey.bogdan@gmail.com>
2025-01-30 23:03:51 +0200

    add homework5 Incorrect format

---
+++ HillelLesson5Homework5IncorrectFormat/HillelLesson5Homework5IncorrectFormat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+double d1;
+while (true)
+{
+    Console.Write("Введіть число:");
+    if (double.TryParse(Console.ReadLine(), out d1))
+    {
+        Console.WriteLine($"{d1}\n");
+    }
+    else
+    {
+        Console.WriteLine("Помилка, введіть число\n");
+    }
+}

61447cb567caeb90a68fd54be1fac760e6d425e4
4Sergey4 <98.sergey.bogdan@gmail.com>
2025-02-18 17:24:36 +0200

    Add homework 10.2

---
+++ HillelLesson10Homework10_1/HillelLesson10Homework10_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int i = 1;
+StringBuilder sb = new StringBuilder();
+sb.AppendLine();
+
+StringBuilder sbb = new StringBuilder();
+
+Console.WriteLine("Введіть заголовок, дату і опис події (щоб вийти натисніть q): ");
+
+while (true)
+{
+   
+    string user = Console.ReadLine();
+    if (user == "q")
+    {
+        break;
+    }
+    sbb.Append($"Подія {i++}: ");
+    sbb.AppendLine(user);
+   
+}
+sb.Append(sbb);
+Console.WriteLine(sb.ToString());

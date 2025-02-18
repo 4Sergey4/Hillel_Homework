626ee5cad6098dd1ba19674b515262939bf96e27
4Sergey4 <98.sergey.bogdan@gmail.com>
2025-02-18 15:07:41 +0200

    Add homework 10.3

---
+++ HillelLesson10Homework10_1/HillelLesson10Homework10_3/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+string str;
+Console.Write("Введіть строку: ");
+str = Console.ReadLine();
+string replacedStr = str.Replace(" ", "");
+Console.WriteLine(replacedStr);

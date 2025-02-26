7be2172e65169cf786d46bbeda7ff904988e1d5b
4Sergey4 <98.sergey.bogdan@gmail.com>
2025-02-26 14:18:18 +0200

    Add homework 11

---
+++ HillelLesson11Homework11/HillelLesson11Homework11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Random random = new Random();
+string[] words = { "собака", "галактика", "манго", "рибалка", "гепард", "яблуко", "дерево" };
+string word = words[random.Next(words.Length)];
+string guessedWord = new string('*', word.Length);
+char letter;
+int attemps = 6;
+
+Console.WriteLine("Вітаю у грі шибениця");
+Console.WriteLine($"Відгадати слово {guessedWord}");
+Console.WriteLine($"У вас є {attemps} спроб");
+while (attemps > 0)
+{
+    Console.Write("Введіть букву: ");
+    letter = Convert.ToChar(Console.ReadLine());
+    if (word.Contains(letter))
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] == letter)
+            {
+                guessedWord = guessedWord.Remove(i, 1).Insert(i, letter.ToString());
+            }
+        }
+        Console.WriteLine($"Відгадане слово: {guessedWord}");
+        if (word == guessedWord)
+        {
+            Console.WriteLine("Ви виграли");
+            break;
+        }
+    }
+    else
+    {
+        attemps--;
+        Console.WriteLine($"Ви не вгадали. У вас залишилось {attemps} спроб");
+    }
+}

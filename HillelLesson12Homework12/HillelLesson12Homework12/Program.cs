7778999096ade59d7babe7f63040da91597dc93e
4Sergey4 <98.sergey.bogdan@gmail.com>
2025-02-26 09:50:19 +0200

    Add homework 12

---
+++ HillelLesson12Homework12/HillelLesson12Homework12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace HillelLesson12Homework12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int n;
+
+            Console.Write("Введіть кількість ітерацій Фібоначчі: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n\tЧисла Фібоначчі:\n");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{Fibonacci(i)} ");
+            }
+        }
+
+        static int Fibonacci(int n)
+        {
+            if( n < 2)
+            {
+                return n; 
+            }
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+    }
+}

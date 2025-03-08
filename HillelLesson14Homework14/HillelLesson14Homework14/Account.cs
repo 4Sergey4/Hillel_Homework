ef3600de9ed85b1e8ac5586fb10a0d2cca05605e
4Sergey4 <98.sergey.bogdan@gmail.com>
2025-03-08 19:08:06 +0200

    Add homework 14

---
+++ HillelLesson14Homework14/HillelLesson14Homework14/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HillelLesson14Homework14
+{
+    internal class Account
+    {
+        private long _account;
+
+        public string Name;
+        public long Balance 
+        { 
+            get
+            {
+                return _account;
+            }
+            set
+            {
+                _account = value;
+            }
+        }
+
+        public Account(string name, long balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+        public void WriteMyInfo()
+        {
+            Console.WriteLine($"Ім'я: {Name}, Баланс: {Balance}$");
+        }
+
+        public long Withdrawal(long amount)
+        {
+            if (amount > Balance)
+            {
+                Console.WriteLine("Баланс власника не може бути менше нуля!");
+                return 0;
+            }
+            Balance -= amount;
+            return amount;
+        }
+
+        public long Deposit(long amount)
+        {
+            if (amount < 0)
+            {
+                return 0;
+            }
+            Balance += amount;
+            return amount;
+        }
+    }
+}
---
+++ HillelLesson14Homework14/HillelLesson14Homework14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace HillelLesson14Homework14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Account joe = new Account("Joe", 4500);
+            Account bob = new Account("Bob", 55000);
+
+            while (true)
+            {
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+                Console.Write("Ведіть суму: ");
+                string input = Console.ReadLine();
+                if (input == "") break;
+                if (long.TryParse(input, out long amount))
+                {
+                    Console.Write("Оберіть власника рахунку: ");
+                    string owner = Console.ReadLine();
+                    if (owner == "Joe")
+                    {
+                        Console.Write("Оберіть операцю(-,+): ");
+                        string operation = Console.ReadLine();
+                        if (operation == "-")
+                        {
+                            joe.Withdrawal(amount);
+                        }
+                        else if (operation == "+")
+                        {
+                            joe.Deposit(amount);
+                        }
+                    }
+                    else if (owner == "Bob")
+                    {
+                        Console.WriteLine("Оберіть операцю(-,+): ");
+                        string operation = Console.ReadLine();
+                        if (operation == "-")
+                        {
+                            bob.Withdrawal(amount);
+                        }
+                        else if (operation == "+")
+                        {
+                            bob.Deposit(amount);
+                        }
+                    }
+                    Console.WriteLine("Для завершення натисніть Enter");
+                }
+
+            }
+        }
+    }
+}
